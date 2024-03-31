67609ce548c3f6877d79df837c3e5894edf7660e
DSerio <davide@serio.cloud>
2024-04-01 00:28:51 +0200

    Updated app_data and config

--- src/app_data.py
+++ src/app_data.py
@@ -9,7 +9,7 @@ k8sService = K8sService()
 env = config_app.get_env_variables()
 
 if os.getenv('K8S_IN_CLUSTER_MODE').lower() == 'true':
-    env_data = asyncio.run(k8sService.get_config_map(namespace=config_app.get_k8s_velero_ui_namespace()))
+    env_data = asyncio.run(k8sService.get_config_map(namespace=config_app.get_k8s_velero_ui_namespace(), configmap_name='velero-api-config'))
 else:
     env_data = config_app.get_env_variables()
 
